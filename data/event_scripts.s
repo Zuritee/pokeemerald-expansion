3e7901716bb09d8a3201b91a94d16e45e7a2bdf0
Zuritee <candylichqwq@gmail.com>
2023-10-14 22:22:31 -0400

    Set new game in caramel town

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1061,3 +1061,5 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/birch_speech.inc"
 
 	.include "data/maps/CaramelTown/scripts.inc"
+
+	.include "data/maps/Player_House_Interior/scripts.inc"
